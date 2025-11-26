06a14d29e5d693796730721a3d21b397d84528b4
Ciaran.Goulding <ciaran.goulding@wise.com>
2025-11-26 15:03:52 +0000

    Update message text again again

--- api/create-meet.js
+++ api/create-meet.js
@@ -170,7 +170,7 @@ export default async (request, response) => {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Click below to join your Google Meet meeting"
+            text: "Click below to join:"
           }
         },
         {
